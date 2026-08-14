2c5258a1574258fe95132ae5614fb8f1010cde2a
arauf <arauf@SMD-CS-WS-117>
2026-08-14 10:15:26 +0400

    updated cicd.tf

--- terraform/cicd.tf
+++ terraform/cicd.tf
@@ -4,7 +4,7 @@ data "aws_caller_identity" "current" {}
 resource "aws_iam_openid_connect_provider" "github" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["ffffffffffffffffffffffffffffffffffffffff"] # AWS ignores this for GitHub now, but the argument is required
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
 }
 
 # The role the pipeline assumes
